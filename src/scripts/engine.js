ec2b45847fb62958e3ee80d6ba297f27a90b8320
Leandro Sargi <leandro.sargi@hotmail.com>
2024-11-16 18:31:27 -0300

    Primeiro commit após testes feitos no navegador

---
+++ src/scripts/engine.js
@@ -0,0 +1,77 @@
+const state = {
+    view: {
+        squares: document.querySelectorAll(".square"),
+        enemy: document.querySelector(".enemy"),
+        timeLeft: document.querySelector("#time-left"),
+        score: document.querySelector("#score"),
+        life: document.querySelector("#life"),
+    },
+    values: {
+        gameVelocity: 1000,
+        hitPosition: 0,
+        result: 0,
+        currentTime: 60,
+        life: 3,
+    },
+    actions: {
+        timerId: setInterval(randomSquare, 1000),
+        countDownTimerId: setInterval(countDown, 1000),
+    },
+};
+
+function countDown() {
+    state.values.currentTime--;
+    state.view.timeLeft.textContent = state.values.currentTime;
+    if(state.values.currentTime <= 0) {
+        clearInterval(state.actions.countDownTimerId);
+        clearInterval(state.actions.timerId);
+        alert("Game Over! O seu resultado foi: " + state.values.result);
+    }
+}
+
+function playSound(audioName) {
+    let audio = new Audio(`./src/sounds/${audioName}.m4a`);
+    audio.volume = 0.2;
+    audio.play();
+}
+
+function randomSquare() {
+    state.view.squares.forEach((square)=>{
+        square.classList.remove("enemy");
+    });
+
+    let randomNumber = Math.floor(Math.random() * 9);
+    let randomSquare = state.view.squares[randomNumber];
+    randomSquare.classList.add("enemy"); 
+    state.values.hitPosition = randomSquare.id;
+}
+
+function addListenerHitBox() {
+    state.view.squares.forEach((square) => {
+        square.addEventListener("mousedown", () => {
+            if(square.id === state.values.hitPosition) {
+                state.values.result++;
+                state.view.score.textContent = state.values.result;
+                state.values.hitPosition = null;
+                playSound("hit");
+            } else {
+                state.values.life--;
+                state.view.life.textContent = `x${state.values.life}`;
+                if (state.values.life <= 0) {
+                    // Quando vidas chegam a 0, fim de jogo
+                    clearInterval(state.actions.countDownTimerId);
+                    clearInterval(state.actions.timerId);
+                    alert("Game Over! Suas vidas acabaram.");
+                    location.reload();
+                }
+            }
+        });
+    });
+}
+
+
+function initialize() {
+    addListenerHitBox();
+}
+
+initialize();
